c592205ea2aeb9850a4419c975dc606a4f25984a
Ryusuke Fujita <49670803+RyusukeFujita@users.noreply.github.com>
2026-02-21 06:25:07 +0900

    Update Get-ImageInfo
    Add Characteristics property

--- OutlookTrace.psm1
+++ OutlookTrace.psm1
@@ -484,6 +484,27 @@ namespace Win32
         }
     }
 
+    // https://learn.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-image_file_header
+    [Flags]
+    public enum IMAGE_FILE_HEADER
+    {
+        IMAGE_FILE_RELOCS_STRIPPED = 0x0001,
+        IMAGE_FILE_EXECUTABLE_IMAGE = 0x0002,
+        IMAGE_FILE_LINE_NUMS_STRIPPED = 0x0004,
+        IMAGE_FILE_LOCAL_SYMS_STRIPPED = 0x0008,
+        IMAGE_FILE_AGGRESIVE_WS_TRIM = 0x0010,
+        IMAGE_FILE_LARGE_ADDRESS_AWARE = 0x0020,
+        IMAGE_FILE_BYTES_REVERSED_LO = 0x0080,
+        IMAGE_FILE_32BIT_MACHINE = 0x0100,
+        IMAGE_FILE_DEBUG_STRIPPED = 0x0200,
+        IMAGE_FILE_REMOVABLE_RUN_FROM_SWAP = 0x0400,
+        IMAGE_FILE_NET_RUN_FROM_SWAP = 0x0800,
+        IMAGE_FILE_SYSTEM = 0x1000,
+        IMAGE_FILE_DLL = 0x2000,
+        IMAGE_FILE_UP_SYSTEM_ONLY = 0x4000,
+        IMAGE_FILE_BYTES_REVERSED_HI = 0x8000
+    }
+
     namespace Mapi
     {
         // https://learn.microsoft.com/en-us/office/client-developer/outlook/mapi/pidtagresourceflags-canonical-property
@@ -12458,8 +12479,14 @@ function Get-ImageInfo {
             default { 'Unknown'; break }
         }
 
+        # Skip next 16 bytes: NumberOfSections (2 bytes), TimeDateStamp (4), PointerToSymbolTable (4), NumberOfSymbols (4), SizeOfOptionalHeader (2)
+        $null = $reader.ReadBytes(16)
+
+        $characteristics = $reader.ReadUInt16()
+
         [PSCustomObject]@{
-            Architecture = $arch
+            Architecture    = $arch
+            Characteristics = [Win32.IMAGE_FILE_HEADER]$characteristics
         }
     }
     catch {
